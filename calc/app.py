2791c1a304219b7c5c318f4d4530a8a920ab14e2
Jason McGinty <jason_mcginty@yahoo.com>
2020-07-22 20:20:12 -0500

    calc app completed

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,30 @@
 # Put your app in here.
+from flask import Flask
+from flask import request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route('/<op>')
+def operate(op):
+    operations = {
+        'add': add,
+        'sub': sub,
+        'mult': mult,
+        'div': div
+    }
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(operations[op](a, b))
+
+@app.route('/math/<op>')
+def operator_the_second(op):
+    operations = {
+        'add': add,
+        'sub': sub,
+        'mult': mult,
+        'div': div
+    }
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(operations[op](a, b))
